5e08c1bca0e8cc021fddce73c91e83e624937c1b
xovest <79008917+xovest@users.noreply.github.com>
2022-03-12 09:27:05 +0200

    made popular picks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Pages from "./pages/Pages";
+
 function App() {
   return (
     <div className="App">
-      <h1>hay</h1>
+      <Pages />
     </div>
   );
 }
---
+++ src/pages/Pages.jsx
@@ -0,0 +1,9 @@
+import Home from "./Home";
+
+function Pages() {
+  return (
+    <Home />
+  )
+}
+
+export default Pages
