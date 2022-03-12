88b5207b3efbd80b84f37e64b52acb742a760c76
xovest <79008917+xovest@users.noreply.github.com>
2022-03-12 16:13:31 +0200

    damn that timeline

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -18,6 +18,7 @@ function Popular() {
     } else {
       const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
       const data = await api.json()
+
       localStorage.setItem('popular', JSON.stringify(data.recipes))
       setPopular(data.recipes)
     }
